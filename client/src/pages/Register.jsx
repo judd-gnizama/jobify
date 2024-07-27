0a27fdc90117d80c7bf770aab3ce23aadb576dbe
judd.gnizama <juddmaverick@gmail.com>
2024-07-27 13:57:42 +0800

    added react-toastify

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,13 +1,30 @@
-import { Form, Link } from "react-router-dom";
+import { Form, Link, redirect, useNavigation } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo } from "../components";
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
 
-export const registerAction = async (data) => {
-  console.log("register action");
-  return null; // must always return anything
+export const registerAction = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    await customFetch.post("/auth/register", data);
+    toast.success("Registration successful");
+    return redirect("/login"); // this redirect only works for actions for forms
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
+
+  // this action function must always return something even if it's just null
 };
 
 const Register = () => {
+  const navigation = useNavigation();
+  console.log(navigation);
+  const isSubmitting = navigation.state === "submitting";
+
   return (
     <Wrapper>
       <Form method="post" className="form">
@@ -23,8 +40,8 @@ const Register = () => {
         <FormRow type="text" name="location" defaultValue="earth" />
         <FormRow type="email" name="email" defaultValue="john@gmail.com" />
         <FormRow type="password" name="password" defaultValue="secret123" />
-        <button type="submit" className="btn btn-block">
-          submit
+        <button type="submit" className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting ? "Submitting" : "submit"}
         </button>
         <p>
           Already a member?
