61e111f01bbc7f4469fc1ed97c121af4b861bdbc
judd.gnizama <juddmaverick@gmail.com>
2024-07-27 15:24:39 +0800

    added add job components

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,5 +1,51 @@
+import { Form, useNavigation, useOutletContext } from "react-router-dom";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import FormRow from "../components/FormRow";
+import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
+import FormRowSelect from "../components/FormRowSelect";
+
 const AddJob = () => {
-  return <h1>AddJob Page</h1>;
+  const { user } = useOutletContext();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
+  return (
+    <Wrapper>
+      <Form method="post" className="form">
+        <h4 className="form-title">add job</h4>
+        <div className="form-center">
+          <FormRow type="text" name="position" />
+          <FormRow type="text" name="company" />
+          <FormRow
+            type="text"
+            name="jobLocation"
+            labelText="job location"
+            defaultValue={user.location}
+          />
+          <FormRowSelect
+            name="jobStatus"
+            labelText="job Status"
+            defaultValue={JOB_STATUS.PENDING}
+            list={Object.values(JOB_STATUS)}
+          />
+          <FormRowSelect
+            name="jobType"
+            labelText="job type"
+            defaultValue={JOB_TYPE.FULL_TIME}
+            list={Object.values(JOB_TYPE)}
+          />
+
+          <button
+            type="submit"
+            className="btn btn-block form-btn"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Submitting" : "submit"}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
 };
 
 export default AddJob;
